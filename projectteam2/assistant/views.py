4b887b5cbfcbe37dc07400a6000e08a1a56bf82c
Oana Cos <oana.patricia.cos@gmail.com>
2021-06-20 12:50:58 +0300

    some function formating and some small changes

--- projectteam2/assistant/views.py
+++ projectteam2/assistant/views.py
@@ -2,21 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Documents
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def index2(request):
-    return HttpResponse("HTESTTT.")
-
-def get_documents_to_expire(request):
-    obj = Documents.objects.get(id=1)
-#    context = {
- #       'doc_name': obj.name,
-  #      'doc_expiry_date': obj.expiry_date
-   # }
-    return HttpResponse(obj.name)
-        #render(request, "document/detail.html", {})
-
 
 def home(request):
     return render(request, 'CarAssistant/homepage.html')
@@ -26,3 +11,19 @@ def sign_up(request):
     return render(request, 'CarAssistant/signup.html')
 
 
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index2(request):
+    return HttpResponse("HTESTTT.")
+
+
+def get_documents_to_expire(request):
+    obj = Documents.objects.get(id=1)
+    #    context = {
+    #       'doc_name': obj.name,
+    #      'doc_expiry_date': obj.expiry_date
+    # }
+    return HttpResponse(obj.name)
+    # render(request, "document/detail.html", {})
